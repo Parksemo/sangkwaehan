dcc613db79d24a5ab1dc5e6deb0d827a1e2333aa
sooooomin84 <soominnm.000804@gmail.com>
2022-06-21 14:02:40 +0900

    ìchat bot done

--- chat_bot.py
+++ chat_bot.py
@@ -4,6 +4,7 @@ from telegram.ext import MessageHandler, Filters
 import pandas as pd
 import telegram
 from CBF import CBF
+from random_keyword import print_random_keyword
 
 token = '5569336973:AAHuk9BSs66Uq2fv7kuwNLCPVshMthsMXvA'
 id = '5541102425'
@@ -24,8 +25,8 @@ df = pd.read_pickle("pickle_review_data_frame")
 '''
 
 # 1 -> 나중에 문구 수정
-bot.sendMessage(chat_id=id, text="추천 시스템 동작")  # 메세지 보내기
-bot.sendMessage(chat_id=id, text="가고 싶은 여행지의 키워드를 입력해봐")
+bot.send_photo(chat_id=id, photo=open('main.PNG', 'rb'))
+bot.sendMessage(chat_id=id, text="가고 싶은 여행지의 키워드를 입력해주세요.")
 
 def handler(update, context):
     # 2
@@ -42,8 +43,13 @@ def handler(update, context):
         for j in honey_tourist_attractions:
             bot.sendMessage(chat_id=id, text=f"꿀 여행지는: {j}")  # 꿀 여행지
 
+    #3-2
     else:
-        bot.sendMessage(chat_id=id, text="없어 이자시가")
+        key_list = print_random_keyword(k_list)
+        bot.sendMessage(chat_id=id, text="검색한 키워드를 찾을 수 없습니다. 시스템의 추천 키워드는 다음과 같습니다.")
+        for k in key_list:
+            bot.sendMessage(chat_id=id, text=f"{k}")
+        bot.sendMessage(chat_id=id, text="다시 가고 싶은 여행지의 키워드를 입력해주세요.")
 
 echo_handler = MessageHandler(Filters.text, handler)
 dispatcher.add_handler(echo_handler)
